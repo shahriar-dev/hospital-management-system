3d57651a8703df60589762fd982790fe6be2793a
Shahriar Abir <abir.sh06@gmail.com>
2021-06-25 10:29:40 +0600

    2nd commit

--- Hospital-Management-System/Patient/registration-patient.php
+++ Hospital-Management-System/Patient/registration-patient.php
@@ -12,13 +12,24 @@
 <body>
     <h1>Patient Resigtration</h1>
     <div style="max-width: 600px;  margin: 0 auto;">
+        <style>
+            table {
+                max-width: 600px;
+                width: 365px;
+                font-size: .8rem;
+            }
+
+            td {
+                width: 280px;
+            }
+        </style>
         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
             <fieldset>
                 <legend align="center">Basic Information</legend>
-                <table align="center">
+                <table style="margin: 0 auto;" border="1px">
                     <style>
                         td {
-                            padding: 10px 30px 10px 30px;
+                            padding: 10px 20px 10px 20px;
                         }
                     </style>
                     <tr>
@@ -53,6 +64,45 @@
                     </tr>
 
                 </table>
+            </fieldset>
+
+            <fieldset>
+                <legend align="center">Contact Informatin</legend>
+                <table align="center" border="1px">
+                    <style>
+                        td {
+                            padding: 10px 20px 10px 20px;
+                        }
+                    </style>
+                    <tr>
+                        <td><label for="input_phonenumber">Phone Number</label></td>
+                        <td><input type="tel" id="input_phonenumber" name="phoneNumber" placeholder="(+88)01xxxxxxxxx"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="input_email">Email</label></td>
+                        <td><input type="email" id="input_email" name="email" placeholder="something@gmail.com"></td>
+                    </tr>
+                </table>
+            </fieldset>
+
+            <fieldset>
+                <legend align="center">Login Details</legend>
+                <table style="margin: 0 auto;" border="1px">
+                    <style>
+                        td {
+                            padding: 10px 20px 10px 20px;
+                        }
+                    </style>
+                    <tr>
+                        <td><label for="input_username">Username</label></td>
+                        <td><input type="text" id="input_usernamme" name="userName" placeholder="Type your username here"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="input_password">Password</label></td>
+                        <td><input type="password" id="input_password" name="password" placeholder="Give a Password"></td>
+                    </tr>
+                </table>
+            </fieldset>
             </fieldset>
 
         </form>
