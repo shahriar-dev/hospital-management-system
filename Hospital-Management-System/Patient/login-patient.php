0009a35af231637e9528bda5fb4ce46cfd40b885
Shahriar Abir <abir.sh06@gmail.com>
2021-06-25 20:48:18 +0600

    3rd commit - login page

---
+++ Hospital-Management-System/Patient/login-patient.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="patient-login">
+    <title>Patient - Login</title>
+
+</head>
+
+<body style="background-image: url(img/undraw_medicine.svg); background-position:cover; background-size:700px 1200px; background-repeat:no-repeat;">
+    <div style="border-radius: 15px 15px 0px 0px; background-color:rgb(0,0,0,6); margin:auto; color:#6460A4; width:800px;">
+        <h2 align="center">Login - Patient</h2>
+    </div>
+
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST" style="width: 30%; margin: 0px auto; padding: 20px; border:1px solid #9A94E0; border-radius: 0px 0px 10px 10px;">
+        <div style="margin: 10px 0px 10px 0px;">
+            <label for="username" style="display: block; margin:3px;">Username</label>
+            <input type="text" name="username" style="display: block; margin:3px; border-radius:5px; height: 30px; width:93%; font-size:16px; border: 2px solid #9A94E0;">
+        </div>
+        <div style="margin: 10px 0px 10px 0px;">
+            <label for="password" style="display: block; margin:3px;">Password</label>
+            <input type="password" name="password" style="display: block; margin:3px; border-radius:5px; height: 30px; width:93%; font-size:16px; border: 2px solid #9A94E0;">
+        </div>
+        <div style="margin: 10px 0px 10px 0px;">
+            <button type="submit" value="submit" style="display: block; margin:3px; padding: 10px; font-size: 14px; background-color: #67BDD2; color: white; border:none; border-radius: 6px;">Login</button>
+        </div>
+
+        <p>
+            Not yet Registered? <a href="registration-patient.php">Sign up</a>
+        </p>
+    </form>
+</body>
+
+</html>
--- Hospital-Management-System/Patient/registration-patient.php
+++ Hospital-Management-System/Patient/registration-patient.php
@@ -9,13 +9,18 @@
     <title>Registration - PATIENT</title>
 </head>
 
-<body>
-    <h1>Patient Resigtration</h1>
+<style>
+</style>
+
+<body style="background-image: url(img/undraw_medicine.svg); background-position:cover; background-size:700px 1200px; background-repeat:no-repeat;">
+    <div style="border-radius: 15px 15px 0px 0px; background-color:rgb(0,0,0,6); margin:auto; color:#6460A4; width:800px;">
+        <h1 align="center">Patient Resigtration</h1>
+    </div>
     <div style="max-width: 600px;  margin: 0 auto;">
         <style>
             table {
                 max-width: 600px;
-                width: 365px;
+                width: 450px;
                 font-size: .8rem;
             }
 
@@ -23,89 +28,104 @@
                 width: 280px;
             }
         </style>
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
-            <fieldset>
-                <legend align="center">Basic Information</legend>
-                <table style="margin: 0 auto;" border="1px">
-                    <style>
-                        td {
-                            padding: 10px 20px 10px 20px;
-                        }
-                    </style>
-                    <tr>
-                        <td><label for="input_firstname">First Name</label></td>
-                        <td><input type="text" id="input_firstname" name="firstName" placeholder="Enter your First Name"></td>
-                    </tr>
-                    <tr>
-                        <td><label for="input_lastname">Last Name</label></td>
-                        <td><input type="text" id="input_lastname" name="lastName" placeholder="Enter your Last Name"></td>
-                    </tr>
-                    <tr>
-                        <td><label for="input_gender">Gender</label></td>
-                        <td>
-                            <input type="radio" id="input_gender" name="gender" value="Male">Male</input>
-                            <input type="radio" id="input_gender" name="gender" value="Female">Female</input>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label for="input_religion">Religion</label></td>
-                        <td>
-                            <select name="religion" id="input_religion" value="">
-                                <option value="default">Select a Value</option>
-                                <option value="muslim">Muslim</option>
-                                <option value="hindu">Hindu</option>
-                                <option value="christian">Christian</option>
-                            </select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label for="input_dob">Date of Birth</label></td>
-                        <td><input type="date" id="input_dob" name="dob"></td>
-                    </tr>
+        <div>
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
+                <div>
+                    <fieldset>
+                        <legend align="center">Basic Information</legend>
+                        <table style="margin: 0 auto;">
+                            <style>
+                                td {
+                                    padding: 10px 20px 10px 20px;
+                                }
+                            </style>
+                            <tr>
+                                <td><label for="input_firstname">First Name</label></td>
+                                <td><input type="text" id="input_firstname" name="firstName" placeholder="Enter your First Name"></td>
+                                <td><label for=""></label></td>
+                            </tr>
+                            <tr>
+                                <td><label for=" input_lastname">Last Name</label></td>
+                                <td><input type="text" id="input_lastname" name="lastName" placeholder="Enter your Last Name"></td>
+                                <td><label for=""></label></td>
+                            </tr>
+                            <tr>
+                                <td><label for=" input_gender">Gender</label></td>
+                                <td>
+                                    <input type="radio" id="input_gender" name="gender" value="Male">Male</input>
+                                    <input type="radio" id="input_gender" name="gender" value="Female">Female</input>
+                                </td>
+                                <td><label for=""></label></td>
+                            </tr>
+                            <tr>
+                                <td><label for="input_religion">Religion</label></td>
+                                <td>
+                                    <select name="religion" id="input_religion" value="">
+                                        <option disabled selected>--Choose a Option--</option>
+                                        <option value=" muslim">Muslim</option>
+                                        <option value="hindu">Hindu</option>
+                                        <option value="christian">Christian</option>
+                                    </select>
+                                </td>
+                                <td><label for=""></label></td>
+                            </tr>
+                            <tr>
+                                <td><label for="input_dob">Date of Birth</label></td>
+                                <td><input type="date" id="input_dob" name="dob"></td>
+                                <td><label for=""></label></td>
+                            </tr>
 
-                </table>
-            </fieldset>
+                        </table>
+                    </fieldset>
+                </div>
 
-            <fieldset>
-                <legend align="center">Contact Informatin</legend>
-                <table align="center" border="1px">
-                    <style>
-                        td {
-                            padding: 10px 20px 10px 20px;
-                        }
-                    </style>
-                    <tr>
-                        <td><label for="input_phonenumber">Phone Number</label></td>
-                        <td><input type="tel" id="input_phonenumber" name="phoneNumber" placeholder="(+88)01xxxxxxxxx"></td>
-                    </tr>
-                    <tr>
-                        <td><label for="input_email">Email</label></td>
-                        <td><input type="email" id="input_email" name="email" placeholder="something@gmail.com"></td>
-                    </tr>
-                </table>
-            </fieldset>
 
-            <fieldset>
-                <legend align="center">Login Details</legend>
-                <table style="margin: 0 auto;" border="1px">
-                    <style>
-                        td {
-                            padding: 10px 20px 10px 20px;
-                        }
-                    </style>
-                    <tr>
-                        <td><label for="input_username">Username</label></td>
-                        <td><input type="text" id="input_usernamme" name="userName" placeholder="Type your username here"></td>
-                    </tr>
-                    <tr>
-                        <td><label for="input_password">Password</label></td>
-                        <td><input type="password" id="input_password" name="password" placeholder="Give a Password"></td>
-                    </tr>
-                </table>
-            </fieldset>
-            </fieldset>
+                <fieldset>
+                    <legend align=" center">Contact Informatin</legend>
+                    <table align="center">
+                        <style>
+                            td {
+                                padding: 10px 20px 10px 20px;
+                            }
+                        </style>
+                        <tr>
+                            <td><label for="input_phonenumber">Phone Number</label></td>
+                            <td><input type="tel" id="input_phonenumber" name="phoneNumber" placeholder="(+88)01xxxxxxxxx"></td>
+                            <td><label for=""></label></td>
+                        </tr>
+                        <tr>
+                            <td><label for=" input_email">Email</label></td>
+                            <td><input type="email" id="input_email" name="email" placeholder="something@gmail.com"></td>
+                            <td><label for=""></label></td>
+                        </tr>
+                    </table>
+                </fieldset>
 
-        </form>
+                <fieldset>
+                    <legend align=" center">Login Details</legend>
+                    <table style="margin: 0 auto;">
+                        <style>
+                            td {
+                                padding: 10px 20px 10px 20px;
+                            }
+                        </style>
+                        <tr>
+                            <td><label for="input_username">Username</label></td>
+                            <td><input type="text" id="input_usernamme" name="userName" placeholder="Type your username here"></td>
+                            <td><label for=""></label></td>
+                        </tr>
+                        <tr>
+                            <td><label for=" input_password">Password</label></td>
+                            <td><input type="password" id="input_password" name="password" placeholder="Give a Password"></td>
+                            <td><label for=""></label></td>
+                        </tr>
+                    </table>
+                </fieldset>
+                </fieldset>
+
+            </form>
+        </div>
+
     </div>
 </body>
 
